de2023e561c298f7cdc717ab8a1b2700fa556258
Bryce Davidson <lifebryce@gmail.com>
2018-08-15 13:51:40 -0700

    fixed github link for package

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name='brycetools',
       version='0.1',
       description='bryce tools',
-      url='http://github.com/storborg/funniest',
+      url='https://github.com/BryceDavidson/brycetools',
       author='Bryce Davidson',
       author_email='Bryce_davidson@icloud.com',
       license='MIT',
