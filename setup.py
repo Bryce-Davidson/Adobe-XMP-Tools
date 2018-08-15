4bfead0c4422c040e73c0276cae6229b67578bf7
Bryce Davidson <lifebryce@gmail.com>
2018-08-15 14:02:59 -0700

    new tutorial setup

--- setup.py
+++ setup.py
@@ -1,11 +1,21 @@
-from setuptools import setup
+import setuptools
 
-setup(name='brycetools',
-      version='0.1',
-      description='bryce tools',
-      url='https://github.com/BryceDavidson/brycetools',
-      author='Bryce Davidson',
-      author_email='Bryce_davidson@icloud.com',
-      license='MIT',
-      packages=['brycetools'],
-      zip_safe=False)
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="brycetools",
+    version="0.0.1",
+    author="Bryce Davidson",
+    author_email="bryce_davidson@icloud.com",
+    description="bryce tools",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/BryceDavidson/brycetools",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
