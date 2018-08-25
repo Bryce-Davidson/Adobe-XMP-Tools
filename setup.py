f1e30475ee53b779ea6e7ed26d3e95a53d7ebcec
Bryce Davidson <lifebryce@gmail.com>
2018-08-25 15:13:19 -0700

    updated version type

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="brycetools",
-    version="0.0.1",
+    version="0.0.2",
     author="Bryce Davidson",
     author_email="bryce_davidson@icloud.com",
     description="bryce tools",
